af79bcf4cf996c090211e811efa3a4b1193741a7
sh20raj <shaswatraj3@gmail.com>
2024-04-02 17:43:14 +0530

    python flask app

---
+++ app.py
@@ -0,0 +1,17 @@
+
+# Create a sample website using Flask
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return ("Hello World")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+    
